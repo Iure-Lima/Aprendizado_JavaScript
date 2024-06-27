8b6bb9d199399fd14866bf92ebfe62220aea2c4c
IureLima <iurelimasilva@gmail.com>
2024-06-27 14:27:09 -0300

    começando a seção 9

---
+++ CursoJS/Secao9/aula97/mod1.js
@@ -0,0 +1,10 @@
+const nome = "Iure";
+const sobrenome = "Lima";
+
+const falaNome = () =>{
+  console.log(nome, sobrenome)
+}
+
+exports.nome = nome;
+exports.sobrenome = sobrenome;
+this.funcao = falaNome;
